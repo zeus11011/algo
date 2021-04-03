d9a6c3f2876f74fea4b7f7aa9a37781b031a466c
Anish Naik <naikanish16@gmail.com>
2021-04-03 22:19:18 +0530

    smwht animated

--- src/binary/Box.js
+++ src/binary/Box.js
@@ -1,8 +1,17 @@
 import "./css/Box.css"
+import {useSpring, animated} from 'react-spring';
+
 function Box(prop){
+  const props = useSpring({
+  to: async (next, cancel) => {
+    await next({opacity: 1, color: '#ffaaee',transform: 'translate3d(0,0px,0)'})
+    await next({opacity: 1, color: 'rgb(14,26,19)'})
+  },
+  from: {opacity: 0, color: 'red',transform: 'translate3d(0,-40px,0)'},
+})
   return(
     // <button className="Box" type="text">{prop.num}<button/>
-    <button className="Box">{prop.num}</button>
+    <animated.div style={props} className="Box">{prop.num}</animated.div>
   )
 }
 
